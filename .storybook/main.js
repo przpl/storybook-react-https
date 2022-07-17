2b772172fccf2bb6a78e57d5ef5d8e3fe4e11005
przpl <9073573+przpl@users.noreply.github.com>
2022-07-17 23:13:14 +0200

    Update template

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
     addons: ["@storybook/addon-essentials"],
     framework: "@storybook/react",
     core: { builder: "webpack5" },
